1d1ac27bf5018e29f8f248102ca2fdb36acc33b5
Priyanko Das <pridas2@WKMZT0210FEC.global.publicisgroupe.net>
2023-02-12 00:54:23 +0530

    Dashboard changes

--- kanban-app/src/App.js
+++ kanban-app/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState } from "react";
+import "./App.css";
+import Dashboard from "./Dashboard";
+import Theme from "./Theme";
+import { BoardProvider } from "./BoardContext";
+
+export const ThemeContext = createContext();
 
 function App() {
+  const [currentTheme, setCurrentTheme] = useState("lightTheme");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeContext.Provider
+      value={{ currentTheme, setCurrentTheme, theme: Theme }}
+    >
+      <BoardProvider>
+        <Dashboard />
+      </BoardProvider>
+    </ThemeContext.Provider>
   );
 }
 
